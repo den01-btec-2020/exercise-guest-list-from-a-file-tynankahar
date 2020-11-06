62f0b92889250905d38e74eb498d49cda8bc77bb
tynankahar <19789@bridgend.ac.uk>
2020-11-06 16:18:19 +0000

    done

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+f= open("data.text"  ,  "r")
+if f.mode == "r": 
+    contents = f.read()
+    print (contents)
+
+
+
+
 
 if __name__ == '__main__':
     main()
